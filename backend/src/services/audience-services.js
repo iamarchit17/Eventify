f4e0bb7eca582c6dfff0a1fe86a42789a6c3d5e5
Archit Agarwal <archit.agarwal@L90Y7YWTLY.local>
2024-01-24 11:46:58 +0530

    Updated Audience, Common, Creator and Event Service

---
+++ backend/src/services/audience-services.js
@@ -0,0 +1,25 @@
+const ApiResponse = require('../utils/api-response')
+
+async function createAudience(audience){
+
+}
+
+async function updateAudience(audienceID, payload){
+
+}
+
+async function getAudienceList(){
+
+}
+
+async function getAudienceById(audienceID){
+
+}
+
+async function deleteAudience(audienceID){
+
+}
+
+module.exports={
+    createAudience, updateAudience, getAudienceList, getAudienceById, deleteAudience
+}
