9638dd6e2c9e354b2ae84643c8bd3e08c18db30a
aryan9721 <avora9721@gmail.com>
2024-01-24 12:05:51 +0530

    sidebar removed

--- Frontend/src/layout/MainLayout/index.js
+++ Frontend/src/layout/MainLayout/index.js
@@ -43,11 +43,10 @@ const MainLayout = () => {
     if (open !== drawerOpen) setOpen(drawerOpen);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [drawerOpen]);
-
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
       <Header open={open} handleDrawerToggle={handleDrawerToggle} />
-      <Drawer open={open} handleDrawerToggle={handleDrawerToggle} />
+      {/* <Drawer open={open} handleDrawerToggle={handleDrawerToggle} /> */}
       <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
         <Toolbar />
         <Breadcrumbs navigation={navigation} title />
