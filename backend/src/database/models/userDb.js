8e63648247fa70e254e51dda15f2ead6a59928c6
om-13 <omchalodiya13@gmail.com>
2024-01-24 14:43:25 +0530

    added audienceDb module

--- backend/src/database/models/userDb.js
+++ backend/src/database/models/userDb.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userDbSchema = new Schema({
-    email: {type:String, required: true},
-    password: {type:String, required: true},
-    role: {type:String, required: true}
+    email: {
+        type: String, 
+        required: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    role: {
+        type: String, 
+        required: true
+    }
 }, {timestamps: true})
 
 const UserDb = mongoose.model('user', userDbSchema)
