d3d235416db60c95733290d44a07704ab72efd90
ishanpandey70 <202051089@iiitvadodara.ac.in>
2024-01-29 15:04:09 +0530

    index.js with all commonAPI And registerAudience

--- Frontend/src/apis/index.js
+++ Frontend/src/apis/index.js
@@ -1,6 +1,32 @@
 import axiosInstance from "./requestInterceptor";
 const BaseURL = 'http://localhost:5000'
-async function login(email, password) {
-    axiosInstance.post(BaseURL+'/api/login',{})
-    
+
+async function loginAPI(email, password) {
+    const response  = await axiosInstance.post(BaseURL+'/api/common/login',{email,password});
+    return response
 }
+async function categoriesAPI() {
+    const response  = await axiosInstance.get(BaseURL+'/api/common/categories');
+    // console.log("The response of categories is  " ,response.data.data)
+    return response.data.data
+}
+async function citiesAPI() {
+    const response  = await axiosInstance.get(BaseURL+'/api/common/cities');
+    // console.log("The response of cities is " ,response.data.data)
+    return response.data.data
+}
+
+//To Do : what to send as parameters , the db model ? If yes then how 
+//the function would be getting input and just return the response.
+async function registerAudienceAPI(audience) {
+    const response  = await axiosInstance.post(BaseURL+'/api/audience',audience);
+    // console.log("The response of cities is " ,response.data.data)
+    return response
+}
+
+// async function 
+
+export {
+    loginAPI ,categoriesAPI , citiesAPI ,registerAudienceAPI
+}
+
